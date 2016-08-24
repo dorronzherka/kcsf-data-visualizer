483b8b48a17c7f49132b16305e6d0be8b625dbdd
Vullkan92 <vullkan.halili@riinvest.net>
2016-08-24 18:17:01 +0200

    #12 - Visualize data

--- static/js/draw-charts.js
+++ static/js/draw-charts.js
@@ -1,4 +1,44 @@
-function displayPieChart(container, chart_type) {
+function displayChart(container, chart_type, data, double_questions) {
+    $("#" + container).empty();
+    chart_type = chart_type.replace("-chart", "");
+
+    var series_data = [];
+    var drilldown_series = [];
+    var duplicates = [];
+    for (var item in data) {
+        var serie = {};
+        if (double_questions) {
+            if (duplicates.indexOf(data[item]['type2']) == -1){
+                serie = getSerieJson(data, item, "type2");
+                serie['drilldown'] = serie['name'];
+                duplicates.push(data[item]['type2']);
+                series_data.push(serie);
+            } else {
+                var index = series_data.map(function(d) { return d['name']; }).indexOf(data[item]['type2']);
+                series_data[index]['y'] += data[item]['count'];
+            }
+        } else {
+            serie = getSerieJson(data, item, "type1");
+            series_data.push(serie);
+        }
+    }
+
+    if (double_questions) {
+        for (var element in series_data){
+            var drilldown_serie = {
+                "name": series_data[element]["name"],
+                "id": series_data[element]["name"],
+                "data": []
+            };
+            for (var json in data) {
+                if (data[json]['type2'] == series_data[element]['name']){
+                    drilldown_serie['data'].push([data[json]['type1'], data[json]['count']])
+                }
+            }
+            drilldown_series.push(drilldown_serie);
+        }
+    }
+
     $('#' + container).highcharts({
         chart: {
             type: chart_type,
@@ -13,43 +53,44 @@ function displayPieChart(container, chart_type) {
             series: {
                 dataLabels: {
                     enabled: true,
-                    format: '{point.name}: {point.y:.1f}%'
+                    format: '{point.name}: {point.percentage:.1f}%'
                 }
             }
         },
 
         tooltip: {
-            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+            headerFormat: '<span style="font-size:15px"></span>',
+            pointFormat: '<span style="font-size: 13px; color:{point.color}">{point.name}</span>: <b style="font-size: 13px; font-weight: bolder;">{point.y} organizations</b><br/>'
         },
         series: [{
             name: 'Brands',
             colorByPoint: true,
-            data: [{
-                name: 'Microsoft Internet Explorer',
-                y: 56.33,
-                drilldown: 'Microsoft Internet Explorer'
-            }, {
-                name: 'Chrome',
-                y: 24.03,
-                drilldown: 'Chrome'
-            }, {
-                name: 'Firefox',
-                y: 10.38,
-                drilldown: 'Firefox'
-            }, {
-                name: 'Safari',
-                y: 4.77,
-                drilldown: 'Safari'
-            }, {
-                name: 'Opera',
-                y: 0.91,
-                drilldown: 'Opera'
-            }, {
-                name: 'Proprietary or Undetectable',
-                y: 0.2,
-                drilldown: null
-            }]
+            data: series_data
+            //data: [{
+            //    name: 'Microsoft Internet Explorer',
+            //    y: 56.33,
+            //    drilldown: 'Microsoft Internet Explorer'
+            //}, {
+            //    name: 'Chrome',
+            //    y: 24.03,
+            //    drilldown: 'Chrome'
+            //}, {
+            //    name: 'Firefox',
+            //    y: 10.38,
+            //    drilldown: 'Firefox'
+            //}, {
+            //    name: 'Safari',
+            //    y: 4.77,
+            //    drilldown: 'Safari'
+            //}, {
+            //    name: 'Opera',
+            //    y: 0.91,
+            //    drilldown: 'Opera'
+            //}, {
+            //    name: 'Proprietary or Undetectable',
+            //    y: 0.2,
+            //    drilldown: null
+            //}]
         }],
         drilldown: {
             drillUpButton: {
@@ -57,71 +98,16 @@ function displayPieChart(container, chart_type) {
                     align: 'right'
                 }
             },
-            series: [{
-                name: 'Microsoft Internet Explorer',
-                id: 'Microsoft Internet Explorer',
-                data: [
-                    ['v11.0', 24.13],
-                    ['v8.0', 17.2],
-                    ['v9.0', 8.11],
-                    ['v10.0', 5.33],
-                    ['v6.0', 1.06],
-                    ['v7.0', 0.5]
-                ]
-            }, {
-                name: 'Chrome',
-                id: 'Chrome',
-                data: [
-                    ['v40.0', 5],
-                    ['v41.0', 4.32],
-                    ['v42.0', 3.68],
-                    ['v39.0', 2.96],
-                    ['v36.0', 2.53],
-                    ['v43.0', 1.45],
-                    ['v31.0', 1.24],
-                    ['v35.0', 0.85],
-                    ['v38.0', 0.6],
-                    ['v32.0', 0.55],
-                    ['v37.0', 0.38],
-                    ['v33.0', 0.19],
-                    ['v34.0', 0.14],
-                    ['v30.0', 0.14]
-                ]
-            }, {
-                name: 'Firefox',
-                id: 'Firefox',
-                data: [
-                    ['v35', 2.76],
-                    ['v36', 2.32],
-                    ['v37', 2.31],
-                    ['v34', 1.27],
-                    ['v38', 1.02],
-                    ['v31', 0.33],
-                    ['v33', 0.22],
-                    ['v32', 0.15]
-                ]
-            }, {
-                name: 'Safari',
-                id: 'Safari',
-                data: [
-                    ['v8.0', 2.56],
-                    ['v7.1', 0.77],
-                    ['v5.1', 0.42],
-                    ['v5.0', 0.3],
-                    ['v6.1', 0.29],
-                    ['v7.0', 0.26],
-                    ['v6.2', 0.17]
-                ]
-            }, {
-                name: 'Opera',
-                id: 'Opera',
-                data: [
-                    ['v12.x', 0.34],
-                    ['v28', 0.24],
-                    ['v27', 0.17],
-                    ['v29', 0.16]
-                ]
-            }]
+            series: drilldown_series
         }
     });
+}
+
+function getSerieJson(data, item, type) {
+    var name = data[item][type];
+    var count = data[item]['count'];
+    return {
+        "name": name,
+        "y": count
+    };
 }
